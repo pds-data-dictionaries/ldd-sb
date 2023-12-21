c3b99114c38feffbe7a6826898934cdc72992b46
acraugh <araugh@umd.edu>
2023-12-21 09:16:42 -0500

    Cleaning up document cruft
    Prepping for initial release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Example Namespace'
+project = 'Small Bodies Namespace'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
